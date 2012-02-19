8f8062f1302d336e7745aa14bc7398b89b54f397
Alden Coleman <waygroovy@yahoo.com>
2012-02-19 10:49:29 -0600

    test2

--- WalkieTalkies/src/com/github/WayGroovy/WalkieTalkies/WalkieTalkies.java
+++ WalkieTalkies/src/com/github/WayGroovy/WalkieTalkies/WalkieTalkies.java
@@ -19,6 +19,7 @@ package com.github.WayGroovy.WalkieTalkies;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+/* A comment */
 
 import java.util.logging.Logger;
 import org.bukkit.configuration.file.YamlConfiguration;
